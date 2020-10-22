9dd51393792d6a103adfb082b9f43bdb68a271a7
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-10-21 23:39:19 -0500

    Initial Commit

---
+++ PaginationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaginationSwiftUI
+//
+//  Created by Kingsley Enoka on 10/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
