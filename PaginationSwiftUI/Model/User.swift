6223c13c681cd41ecd0bf25caceab1077525e181
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-05 22:22:45 -0600

    first commit

---
+++ PaginationSwiftUI/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  PaginationSwiftUI
+//
+//  Created by Kingsley Enoka on 10/21/20.
+//
+
+import Foundation
---
+++ PaginationSwiftUI/ViewModel/SearchUser.swift
@@ -0,0 +1,20 @@
+//
+//  SearchUser.swift
+//  PaginationSwiftUI
+//
+//  Created by Kingsley Enoka on 10/22/20.
+//
+
+import SwiftUI
+
+struct SearchUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchUser_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchUser()
+    }
+}
